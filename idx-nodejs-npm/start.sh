60441af3a85a603cf23185a51bb81581666a7ebb
yutian81 <yutian88881@hotmail.com>
2025-10-28 21:29:21 +0800

    Update start.sh

--- idx-nodejs-npm/start.sh
+++ idx-nodejs-npm/start.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# 配置环境变量
 export UUID=""
 export NEZHA_SERVER=""
 export NEZHA_KEY=""
@@ -7,4 +8,6 @@ export ARGO_DOMAIN=""
 export ARGO_AUTH=""
 export CFIP="cf.090227.xyz"
 export NAME="IDX"
+
+# 启动服务
 npx nodejs-argo
