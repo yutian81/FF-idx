15659da9ad2744b9801336a19099bde8106b0284
yutian81 <yutian88881@hotmail.com>
2025-11-21 10:21:07 +0800

    Update start.sh

--- idx-nodejs-npm/start.sh
+++ idx-nodejs-npm/start.sh
@@ -1,26 +1,24 @@
 #!/usr/bin/env bash
 
-TARGET_MODULE="nodejs-argo"
+export SUB_TYPE="${SUB_TYPE:-'nodejs-argo'}"
+export UUID="${UUID:-'32f88fd1-fb7b-489f-ba09-a1c3d55f5294'}"
+export NEZHA_SERVER="${NEZHA_SERVER:-'sos.de5.net:8008'}"
+export NEZHA_KEY="${NEZHA_KEY:-''}"
+export ARGO_DOMAIN="${ARGO_DOMAIN:-''}"
+export ARGO_AUTH="${ARGO_AUTH:-''}"
+export CFIP="${CFIP:-'cf.090227.xyz'}"
+export NAME="${NAME:-'IDX'}"
 
 # 安装依赖
 if [ ! -d "node_modules" ]; then
-  echo "node_modules 目录不存在，正在安装 ${TARGET_MODULE}..."
-  npm install "${TARGET_MODULE}"
-elif ! npm list "${TARGET_MODULE}" --depth=0 >/dev/null 2>&1; then
-  echo "检测到 ${TARGET_MODULE} 未安装或版本不匹配，正在安装 ${TARGET_MODULE}..."
-  npm install "${TARGET_MODULE}"
+  echo "node_modules 目录不存在，正在安装 ${SUB_TYPE}..."
+  npm install "${SUB_TYPE}"
+elif ! npm list "${SUB_TYPE}" --depth=0 >/dev/null 2>&1; then
+  echo "检测到 ${SUB_TYPE} 未安装或版本不匹配，正在安装 ${SUB_TYPE}..."
+  npm install "${SUB_TYPE}"
 else
-  echo "${TARGET_MODULE} 依赖已安装且版本匹配，跳过 npm install"
+  echo "${SUB_TYPE} 依赖已安装且版本匹配，跳过 npm install"
 fi
 
-# 配置环境变量
-export UUID=""
-export NEZHA_SERVER=""
-export NEZHA_KEY=""
-export ARGO_DOMAIN=""
-export ARGO_AUTH=""
-export CFIP="cf.090227.xyz"
-export NAME="IDX"
-
 # 启动服务
-npx "${TARGET_MODULE}"
+npx "${SUB_TYPE}"
